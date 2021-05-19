ea79ff2c23edad399f0141aa78d754b164aea39b
Aleks Kenins <alekskenins@gmail.com>
2021-05-19 11:02:41 -0400

    Added features for Tempest Domain cleric.

--- api/import.sh
+++ api/import.sh
@@ -1,3 +1,3 @@
 ls -1 *.json | sed 's/.json$//' | while read col; do
-    mongoimport --uri "mongodb+srv://admin:CohibaRedMongo*11@cluster0.ykkzj.mongodb.net/dndBeyondClone" --collection subRaces < $col.json;
+    mongoimport --uri "mongodb+srv://admin:CohibaRedMongo*11@cluster0.ykkzj.mongodb.net/dndBeyondClone" --collection features < $col.json;
 done
